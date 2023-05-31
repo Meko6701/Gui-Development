3a9b5a718bbf9e9fab2ec71500f0cc9460f54e70
Malek Abouelkhair <malekgamal16@gmail.com>
2023-05-31 18:59:05 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import ttk
+
+
+def greet():
+    print("Hello, world")
+
+root = tk.Tk()
+root.title("Hello")
+
+greet_button = ttk.Button(root, text= "Greet", command=greet)
+greet_button.pack(side="left", fill = "x", expand = True)
+
+quit_button = ttk.Button(root, text = "Quit", command = root.destroy)
+quit_button.pack(side = "left", fill = "x", expand= True)
+root.mainloop()
---
+++ pack.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+from tkinter import ttk
+
+root = tk.Tk()
+
+main = ttk.Frame(root)
+main.pack(side = "left", fill="both", expand =True)
+
+tk.Label(main, text="Label top", bg= "red").pack(side="top", fill="both", expand= True)
+tk.Label(main, text="Label top", bg= "red").pack(side="top", fill="both",expand= True)
+
+tk.Label(root, text= "Label left", bg= "green").pack(
+    side = "left",expand= True, fill = "both"
+)
+
+root.mainloop()
